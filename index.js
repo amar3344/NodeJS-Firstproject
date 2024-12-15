c4b4de81141687f04735f08beb4867971f7c2d57
amar <amarkumarreddynew@gmail.com>
2024-12-15 22:23:07 +0530

    my first nodejs expressjs mongodb

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+
+mongoose.connect("mongodb://127.0.0.1:27017/test").then(() => {
+  app.listen(3000);
+  console.log("Connected!");
+});
+
+app.get("/", (request, response) => {
+  response.send("hello amar");
+});
+
+app.get("/date", (request, response) => {
+  const date = new Date();
+  response.send(`Today date : ${date}`);
+});
+
+app.post("/api/products", async (request, response) => {
+  try {
+    const product = await Product.create(request.body);
+    response.status(201).json(product);
+  } catch (errors) {
+    response.status(500).json({ message: `amar erros ${errors.message}` });
+    return;
+  }
+});
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
---
+++ models/product.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    quanity: {
+      type: Number,
+      required: true,
+      defaultValue: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      defaultValue: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },{
+    versionKey: false,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
