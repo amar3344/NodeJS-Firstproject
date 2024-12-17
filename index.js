199513d232557938f4d0ff35ef643e5341220ef8
amar kumar <amarkumarreddynew@gmail.com>
2024-12-17 08:35:47 +0530

    update the PUT and DELETE methods of a product

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ mongoose.connect("mongodb+srv://amarkumarreddynew:zjq9aCiwMKeWNhSb@firstdb.l5hma
   console.log("Connected!");
 });
 
-app.get("/", (request, response) => {
-  response.send("hello amar");
-});
-
 app.post("/api/products", async (request, response) => {
   try {
     const product = await Product.create(request.body);
@@ -23,6 +19,48 @@ app.post("/api/products", async (request, response) => {
   }
 });
 
-app.get("/page", (request, response) => {
-  response.sendFile("./page.html", { root: __dirname });
+app.get("/api/products", async (request, response) => {
+  try{
+    const products = await Product.find({})
+    response.status(200).json(products);
+  }catch(errors) {
+    response.status(500).json({ message: `amar erros ${errors.message}` });
+    return;
+  }
+})
+
+app.get("/api/product/:id",async (request,response) => {
+  try{
+    const product = await Product.findById(request.params.id);
+    response.status(200).json(product);
+  }catch(errors) {
+    response.status(500).json({ message: `amar erros ${errors.message}` });
+    return;
+  }
+})
+
+app.put("/api/product/:id", async(req, res) => {
+  try{
+    const product = await Product.findByIdAndUpdate(req.params.id,req.body);
+    if(!product){
+      return res.status(404).json({message: "Product not found"})
+    }
+    const updateProduct = await Product.findById(req.params.id)
+    res.status(200).json(updateProduct);
+  }catch(error){
+    res.status(500).json({ message: `amar erros ${error.message}` });
+  }
 });
+
+// DELETE API
+app.delete("/api/product/:id", async(req,res)=>{
+  try{
+    const product = await Product.findByIdAndDelete(req.params.id)
+    if(!product){
+      return res.status(404).json({message: "Product not found"})
+    }
+    res.status(200).json({message : "Product deleted"});
+  }catch(error){
+    res.status(500).json({ message: `amar erros ${error.message}` });
+  }
+})
